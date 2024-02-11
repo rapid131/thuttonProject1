61049da443b551cf0a9e6beb44f35bbb4e375659
thutton2 <thutton2@csc386.univ.missouriwestern.edu>
2024-02-11 02:15:52 -0600

    committing project 1 to github

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">>>")
+		userinput, _ := scanner.ReadString('\n')
+		args := strings.Fields(userinput)
+		switch args[0] {
+		case "exit":
+			os.Exit(0)
+		case "cd":
+			if len(args) < 2 {
+				//Got this from here https://stackoverflow.com/questions/46028707/how-to-change-the-current-directory-in-go
+				home, _ := os.UserHomeDir()
+				err := os.Chdir(home)
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				os.Chdir(args[1])
+			}
+		case "whoami":
+			fmt.Println("thutton2 Thomas Hutton")
+		default:
+			// got line 33 from ling 33 here https://gist.github.com/bxcodec/da11f6e398981cbf49ceb265859f7997
+			command := exec.Command(args[0], args[1:]...)
+			out, err := command.Output()
+			if err != nil {
+				fmt.Println("Need valid args or command invalid")
+			}
+			fmt.Print(string(out))
+		}
+
+	}
+
+}
