7e2b4c197b1e3c5f48d4d0964aee52516752768d
thutton2 <thutton2@csc386.univ.missouriwestern.edu>
2024-02-11 02:37:03 -0600

    committing new files

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 		case "whoami":
 			fmt.Println("thutton2 Thomas Hutton")
 		default:
-			// got line 33 from ling 33 here https://gist.github.com/bxcodec/da11f6e398981cbf49ceb265859f7997
+			// This command I understood from here https://stackoverflow.com/questions/22781788/how-could-i-pass-a-dynamic-set-of-arguments-to-gos-command-exec-command
 			command := exec.Command(args[0], args[1:]...)
 			out, err := command.Output()
 			if err != nil {
